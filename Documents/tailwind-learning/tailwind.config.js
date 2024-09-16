1dc7b51b811e4be13492e48ecb3731ff3726d017
daryramadhan_ <daryramadhan23@gmail.com>
2024-09-16 16:09:30 +0700

    Learned padding and margin, also the custom styles method

--- Documents/tailwind-learning/tailwind.config.js
+++ Documents/tailwind-learning/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'custom-padding' : '120px'
+      }
+    },
   },
   plugins: [],
 }
